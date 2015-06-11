b1fc51fc1732072832b3965f92a5ac235899ce54
Alexander Höreth <a.hoereth@gmail.com>
2015-06-11 18:40:03 +0200

    Update npm dependencies and introduce gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,18 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var readme = require('gulp-readme-to-markdown');
 var del    = require('del');
+var path   = require('path');
+var merge  = require('merge-stream');
 
 
-// run everything, then watch for changes
+// Return the foldername of a given file path.
+function getSubdir(file) {
+  var dir = path.dirname(path.normalize(file));
+  return ('.' === dir) ? '' : dir;
+}
+
+
+// Run less, js and readme task and watch them for changes afterwards.
 gulp.task('default', ['less', 'js', 'readme'], function() {
   gulp.watch(['./js/*.js', '!./js/*.min.js'], ['js']);
   gulp.watch('./styles/*.less', ['less']);
@@ -16,13 +25,7 @@ gulp.task('default', ['less', 'js', 'readme'], function() {
 });
 
 
-// remove minified js and compiled less/css
-gulp.task('clean', function(cb) {
-  del([ './styles/*.css', './js/*.min.js' ], cb);
-});
-
-
-// all *.less files in ./styles are compiled and minified and saved as *.css
+// Compile and minify all *.less files in ./styles as *.css
 gulp.task('less', function () {
   return gulp.src(['./styles/*.less', '!**/_*.less'])
     .pipe(less({
@@ -32,7 +35,7 @@ gulp.task('less', function () {
 });
 
 
-// all *.js files in ./js are minified and saved as *.min.js
+// Minify all *.js files in ./js as *.min.js
 gulp.task('js', function() {
   return gulp.src([ './js/*.js', '!./js/*.min.js' ])
     .pipe(uglify())
@@ -42,6 +45,8 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./js'));
 });
 
+
+// Generate README.md etc from the WordPress readme.txt
 gulp.task('readme', function() {
   return gulp.src([ 'readme.txt' ])
     .pipe(readme({
@@ -53,4 +58,33 @@ gulp.task('readme', function() {
       }
     }))
     .pipe(gulp.dest('.'));
+});
+
+
+// Copy everything needed for a SVN release to the /build directory.
+gulp.task('build', ['js', 'less', 'readme'], function() {
+  function copy() {
+    var sources = [
+      'img/*',
+      'js/*',
+      'lng/*',
+      'php/*',
+      'styles/*.css',
+      '*.php',
+      '*.txt',
+      '*.md'
+    ];
+
+    var streams = [], stream;
+    sources.forEach(function(source) {
+      var dir = getSubdir(source);
+
+      stream = gulp.src(source).pipe(gulp.dest('./build/' + dir));
+      streams.push(stream);
+    });
+
+    return merge(streams);
+  }
+
+  return del('./build', copy);
 });
