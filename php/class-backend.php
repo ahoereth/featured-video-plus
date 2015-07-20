ahoereth/featured-video-plus

commit 87ef469e5daa628c99b66ce9ad2525c7ba4f6897

Allow removing of foreign featured images. https://wordpress.org/support/topic/cannot-remove-featured-image-if-a-featured-video-is-set

php/class-backend.php
@@ -488,12 +488,14 @@ class FVP_Backend extends Featured_Video_Plus {
 	 *                         the FVP image attachment ID.
 	 */
 	private function delete_featured_image( $post_id, $meta ) {
+		// Remove featured image.
+		delete_post_meta( $post_id, '_thumbnail_id' );
+
+		// If the image is from the current video we do some more fancy stuff
+		// about removing it.
 		if ( empty( $meta['img'] ) ) {
 			return false;
 		}
-
-		// Unset featured image if it is from this video
-		delete_post_meta( $post_id, '_thumbnail_id', $meta['img'] );
 
 		// Check if other posts use the image, if not we can delete it completely
 		$other = self::get_post_by_custom_meta( '_thumbnail_id', $meta['img'] );

commit 6c4997cf2c2a7be90f829001f94deebac79bde5f

Don't remove image if its not connected to the video. Bug introduced in 87ef469.

php/class-backend.php
@@ -329,7 +329,9 @@ class FVP_Backend extends Featured_Video_Plus {
 		// there was a video and we want to delete it
 		if ( ! empty( $meta['full'] ) && empty( $url ) ) {
 			delete_post_meta( $post['id'], '_fvp_video' );
-			$this->delete_featured_image( $post['id'], $meta );
+			if ( get_post_thumbnail_id( $post['id'] ) == $meta['img'] ) {
+				$this->delete_featured_image( $post['id'], $meta );
+			}
 			return false;
 		}
 
@@ -512,8 +514,8 @@ class FVP_Backend extends Featured_Video_Plus {
 		// Remove featured image.
 		delete_post_meta( $post_id, '_thumbnail_id' );
 
-		// If the image is from the current video we do some more fancy stuff
-		// about removing it.
+		// If the image is a featured video thumbnail we might want to remove it
+		// completely from the media library.
 		if ( empty( $meta['img'] ) ) {
 			return false;
 		}