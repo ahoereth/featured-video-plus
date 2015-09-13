2f1da71e2714a3ca3bc1727563125a097ae94c8c
Alexander Höreth <a.hoereth@gmail.com>
2015-09-13 14:39:34 +0200

    Handle shortcode and post thumbnail filtering on front- AND backend.

--- php/class-frontend.php
+++ php/class-frontend.php
@@ -6,9 +6,7 @@ require_once( FVP_DIR . 'php/class-main.php' );
 /**
  * Class containing frontend functionality.
  *
- * Enqueue scripts/styles, replace featured images by featured videos or
- * insert the ajax request handlers, add 'has-post-video' class and
- * register the [featured-video-plus] shortcode.
+ * Enqueue scripts and styles specific to the frontend.
  *
  * @since 1.0.0
  */
@@ -23,11 +21,6 @@ class FVP_Frontend extends Featured_Video_Plus {
 		parent::__construct();
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
-
-		add_filter( 'post_thumbnail_html', array( $this, 'filter_post_thumbnail' ), 99, 5 );
-		add_filter( 'post_class', array( $this, 'has_post_video_class' ) );
-
-		add_shortcode( 'featured-video-plus', array( $this, 'shortcode' ) );
 	}
 
 
@@ -117,141 +110,4 @@ class FVP_Frontend extends Featured_Video_Plus {
 	}
 
 
-	/**
-	 * Display featured videos in place of featured images if a featured video is available and only if so desired by user.
-	 *
-	 * @see http://wordpress.stackexchange.com/a/41858
-	 * @since 1.0.0
-	 *
-	 * @param string $html featured image html, ready to echo
-	 * @param int $post_id id of target post
-	 * @param int $post_thumbnail_id id of featured image
-	 * @param string|array $size desired size of featured image / video
-	 * @param array $attr
-	 */
-	public function filter_post_thumbnail(
-		$html,
-		$post_id,
-		$post_thumbnail_id,
-		$size,
-		$attr
-	) {
-		$size = $this->get_size();
-
-		$options = get_option( 'fvp-settings' );
-		$mode = ! empty( $options['mode'] ) ? $options['mode'] : null;
-		$conditions = ! empty( $options['conditions'] ) ?
-			$options['conditions'] : null;
-		$single_replace = is_single() &&
-			! empty( $options['single_replace'] ) && $options['single_replace'];
-
-		// Don't show a video.
-		if ( ( 'manual' === $mode ) ||
-		     ( ! self::check_conditions( $conditions ) ) ||
-		     ( ! has_post_video( $post_id ) )
-		) {
-			return $html;
-		}
-
-
-		// Playicon with onload JavaScript for initalizing FVP JS functionality
-		// which has to be done from here because of infinite scroll plugins.
-		$onload = '<img class="playicon onload" ' .
-		            'src="'. FVP_URL . 'img/playicon.png" ' .
-		            'alt="Featured Video Play Icon" ' .
-		            'onload="(function() {' .
-		              "('initFeaturedVideoPlus' in this) && ".
-		              "('function' === typeof initFeaturedVideoPlus) && ".
-		              "initFeaturedVideoPlus();" .
-		            '})();" ' .
-		          '/>';
-
-		// Show the video on-click - lazy load.
-		if ( 'dynamic' === $mode && ! $single_replace ) {
-			return sprintf(
-				'<a href="#" data-id="%s" class="fvp-dynamic post-thumbnail">%s</a>%s',
-				$post_id,
-				$html,
-				$onload
-			);
-		}
-
-		// Show the video on-click in an overlay.
-		if ( 'overlay' === $mode && ! $single_replace ) {
-			return sprintf(
-				'<a href="#" data-id="%s" class="fvp-overlay post-thumbnail">%s</a>%s',
-				$post_id,
-				$html,
-				$onload
-			);
-		}
-
-		// Replace the featured image with the video.
-		return get_the_post_video( $post_id, $size ) . $onload;
-	}
-
-
-	/**
-	 * Add a 'has-post-video' class to posts if appropriate.
-	 *
-	 * @since 2.0.0
-	 *
-	 * @param  {array} $classes Existing classes
-	 * @return {array}          Updated classes
-	 */
-	public function has_post_video_class( $classes ) {
-		global $post;
-
-		if ( has_post_video( $post->ID ) ) {
-			$classes[] = 'has-post-video';
-		}
-		return $classes;
-	}
-
-
-	/**
-	 * Shortcode for usage in post or page entries. Echos the post's featured video.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param array $atts can contain the width and/or height how the featured video should be displayed in px, optional
-	 */
-	public function shortcode($atts){
-		$w = isset($atts['width'])  ? $atts['width'] : '';
-		$h = isset($atts['height']) ? $atts['height'] : '';
-
-		if ( has_post_video() ) {
-			return get_the_post_video( null, array( $w, $h ) );
-		}
-	}
-
-
-	/**
-	 * Check a given set of display conditions if one or more of them hold. If
-	 * an empty set is given, return true.
-	 *
-	 * @param {assoc} $conditions
-	 * @return {bool}
-	 */
-	private static function check_conditions( $conditions ) {
-		if ( empty( $conditions ) ) {
-			return true;
-		}
-
-		$conditions_hold = false;
-		foreach ( $conditions AS $fun => $value ) {
-			$negate = false;
-			if ( '!' === $fun[0] ) {
-				$negate = true;
-				$fun = substr( $fun, 1 );
-			}
-
-			if ( $value && function_exists( 'is_' . $fun ) ) {
-				$call = call_user_func( 'is_' . $fun );
-				$conditions_hold = $conditions_hold || ( $negate ? ! $call : $call );
-			}
-		}
-
-		return $conditions_hold;
-	}
 }
--- php/class-main.php
+++ php/class-main.php
@@ -4,7 +4,9 @@
 require_once( FVP_DIR . 'php/class-html.php' );
 
 /**
- * Class containing all functions needed on front- AND backend. Functions only needed on one of those are found in distinct classes.
+ * Class containing all functions needed on front- AND backend. Functions only
+ * needed on one of those are found in the individual FVP_Frontend and
+ * FVP_Backend classes.
  *
  * @since 1.0.0
  */
@@ -16,6 +18,13 @@ class Featured_Video_Plus {
 		$this->oembed = new FVP_oEmbed();
 
 		add_action( 'plugins_loaded', array( $this, 'language' ) );
+
+		add_shortcode( 'featured-video-plus', array( $this, 'shortcode' ) );
+
+		// Mainly frontend stuff, but lives here because it also needs to be
+		// available on the backend because thats where AJAX requests are processed.
+		add_filter( 'post_thumbnail_html', array( $this, 'filter_post_thumbnail' ), 99, 5 );
+		add_filter( 'post_class', array( $this, 'has_post_video_class' ) );
 	}
 
 
@@ -27,7 +36,11 @@ class Featured_Video_Plus {
 	 * @param int $post_id
 	 * @param string|array $size
 	 */
-	public function get_the_post_video( $post_id = null, $size = null ) {
+	public function get_the_post_video(
+		$post_id = null,
+		$size = null,
+		$ajax = null
+	) {
 		$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 
 		if ( ! has_post_video( $post_id ) ) {
@@ -43,7 +56,7 @@ class Featured_Video_Plus {
 		$general = ! empty( $defaults['general'] ) ? $defaults['general'] : array();
 
 		// Autoplay option. Suppressed when viewing admin.
-		$general['autoplay'] = self::parse_autoplay_options($options) ? true : null;
+		$general['autoplay'] = self::parse_autoplay_options( $options, $ajax );
 
 		// Responsive scaling option. Not used when viewing the admin screen.
 		$responsive =
@@ -63,7 +76,7 @@ class Featured_Video_Plus {
 		switch ( $provider ) {
 			case 'local':
 				$img_meta = wp_get_attachment_metadata( $meta['id'] );
-				$size = $this->get_size( $size, array(
+				$size = self::get_size( $size, array(
 					'width'  => ! empty($img_meta['width'] ) ? $img_meta['width']  : null,
 					'height' => ! empty($img_meta['height']) ? $img_meta['height'] : null,
 				) );
@@ -90,7 +103,7 @@ class Featured_Video_Plus {
 			default:
 				$atts = array_merge(
 					$general,
-					$this->get_size( $size ),
+					self::get_size( $size ),
 					! empty( $defaults[ $provider ] ) ? $defaults[ $provider ] : array(),
 					isset( $meta['parameters'] ) ? $meta['parameters'] : array()
 				);
@@ -125,6 +138,130 @@ class Featured_Video_Plus {
 
 
 	/**
+	 * Shortcode for usage in post or page entries. Echos the post's featured video.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $atts width and height specifications, optional
+	 */
+	public function shortcode( $atts = null ) {
+		$w = isset( $atts['width'] )  ? $atts['width']  : '';
+		$h = isset( $atts['height'] ) ? $atts['height'] : '';
+
+		if ( has_post_video() ) {
+			return get_the_post_video( null, array( $w, $h ) );
+		}
+	}
+
+
+	/**
+	 * Filter the post thumbnail to eventually replace it with the
+	 * featured video.
+	 *
+	 * @see http://wordpress.stackexchange.com/a/41858
+	 * @since 1.0.0
+	 *
+	 * @param string $html featured image html, ready to echo
+	 * @param int $post_id id of target post
+	 * @param int $post_thumbnail_id id of featured image
+	 * @param string|array $size desired size of featured image / video
+	 * @param array $attr
+	 */
+	public function filter_post_thumbnail(
+		$html,
+		$post_id,
+		$post_thumbnail_id,
+		$size,
+		$attr
+	) {
+		$size = self::get_size();
+
+		$options = get_option( 'fvp-settings' );
+		$mode = ! empty( $options['mode'] ) ? $options['mode'] : null;
+		$conditions = ! empty( $options['conditions'] ) ?
+			$options['conditions'] : null;
+		$single_replace = is_single() &&
+			! empty( $options['single_replace'] ) && $options['single_replace'];
+
+		// Don't show a video.
+		if ( ( 'manual' === $mode ) ||
+		     ( ! self::check_conditions( $conditions ) ) ||
+		     ( ! has_post_video( $post_id ) )
+		) {
+			return $html;
+		}
+
+
+		// Playicon with onload JavaScript for initalizing FVP JS functionality
+		// which has to be done from here because of infinite scroll plugins.
+		$onload = '<img class="playicon onload" ' .
+		            'src="'. FVP_URL . 'img/playicon.png" ' .
+		            'alt="Featured Video Play Icon" ' .
+		            'onload="(function() {' .
+		              "('initFeaturedVideoPlus' in this) && ".
+		              "('function' === typeof initFeaturedVideoPlus) && ".
+		              "initFeaturedVideoPlus();" .
+		            '})();" ' .
+		          '/>';
+
+		// Show the video on-click - lazy load.
+		if ( 'dynamic' === $mode && ! $single_replace ) {
+			return sprintf(
+				'<a href="#" data-id="%s" class="fvp-dynamic post-thumbnail">%s</a>%s',
+				$post_id,
+				$html,
+				$onload
+			);
+		}
+
+		// Show the video on-click in an overlay.
+		if ( 'overlay' === $mode && ! $single_replace ) {
+			return sprintf(
+				'<a href="#" data-id="%s" class="fvp-overlay post-thumbnail">%s</a>%s',
+				$post_id,
+				$html,
+				$onload
+			);
+		}
+
+		// Replace the featured image with the video.
+		return get_the_post_video( $post_id, $size ) . $onload;
+	}
+
+
+	/**
+	 * Add a 'has-post-video' class to posts if appropriate.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param  {array} $classes Existing classes
+	 * @return {array}          Updated classes
+	 */
+	public function has_post_video_class( $classes ) {
+		global $post;
+
+		if ( has_post_video( $post->ID ) ) {
+			$classes[] = 'has-post-video';
+		}
+		return $classes;
+	}
+
+
+	/**
+	 * Initializes i18n
+	 *
+	 * @since 1.3.0
+	 */
+	public function language() {
+		load_plugin_textdomain(
+			'featured-video-plus',
+			FVP_DIR . 'lng/',
+			FVP_NAME . '/lng/'
+		);
+	}
+
+
+	/**
 	 * Determine featured video size
 	 *
 	 * @since  1.4.0
@@ -136,7 +273,7 @@ class Featured_Video_Plus {
 	 * @return {array}        The desired video size also taking the options set
 	 *                        in the media settings into consideration.
 	 */
-	protected function get_size( $size = null, $original = null ) {
+	protected static function get_size( $size = null, $original = null ) {
 		$options = get_option( 'fvp-settings' );
 
 		// fixed size requested as array( width => #, height => # ) or array( #, # )
@@ -144,7 +281,7 @@ class Featured_Video_Plus {
 				$width = isset( $size['width'] ) && is_numeric( $size['width'] ) ?
 					$size['width'] :
 					( isset( $size[0] ) && is_numeric( $size[0] ) ? $size[0] : null );
-				$height = isset( $size['height'] ) &&is_numeric( $size['height'] ) ?
+				$height = isset( $size['height'] ) && is_numeric( $size['height'] ) ?
 					$size['height'] :
 					( isset( $size[1] ) && is_numeric( $size[1] ) ? $size[1] : null );
 
@@ -189,48 +326,6 @@ class Featured_Video_Plus {
 
 
 	/**
-	 * Parse the autoplay options to determine if video should or should not
-	 * autoplay.
-	 *
-	 * @param  {assoic} $options
-	 * @return {bool}
-	 */
-	private static function parse_autoplay_options( $options ) {
-		if ( empty( $options['autoplay'] ) ) {
-			return false;
-		}
-
-		if (
-			! empty( $options['autoplay']['always'] ) &&
-			$options['autoplay']['always']
-		) {
-			return true;
-		};
-
-		$mode = ! empty( $options['mode'] ) ? $options['mode'] : null;
-		//$islazy = 'overlay' === $mode || 'dynamic' === $mode;
-		$isajax = defined( 'DOING_AJAX' ) && DOING_AJAX;
-
-		if (
-			! empty( $options['autoplay']['lazy'] ) &&
-			$options['autoplay']['lazy'] &&
-			$isajax
-		) {
-			return true;
-		}
-
-		if (
-			! empty( $options['autoplay']['single'] ) &&
-			$options['autoplay']['single'] &&
-			is_single()
-		) {
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
 	 * Gets a post by an meta_key meta_value pair. Returns it's post_id.
 	 *
 	 * @see http://codex.wordpress.org/Class_Reference/wpdb
@@ -247,7 +342,8 @@ class Featured_Video_Plus {
 		global $wpdb;
 		if ( $meta_value !== null ) {
 			$prepared = $wpdb->prepare(
-				"SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key=%s AND meta_value=%s LIMIT 1",
+				"SELECT post_id FROM {$wpdb->postmeta} " .
+					"WHERE meta_key=%s AND meta_value=%s LIMIT 1",
 				$meta_key,
 				$meta_value
 			);
@@ -263,15 +359,75 @@ class Featured_Video_Plus {
 
 
 	/**
-	 * Initializes i18n
+	 * Check a given set of display conditions if one or more of them hold. If
+	 * an empty set is given, return true.
 	 *
-	 * @since 1.3.0
+	 * @param {assoc} $conditions
+	 * @return {bool}
 	 */
-	public function language() {
-		load_plugin_textdomain(
-			'featured-video-plus',
-			FVP_DIR . 'lng/',
-			FVP_NAME . '/lng/'
-		);
+	private static function check_conditions( $conditions ) {
+		if ( empty( $conditions ) ) {
+			return true;
+		}
+
+		$conditions_hold = false;
+		foreach ( $conditions AS $fun => $value ) {
+			$negate = false;
+			if ( '!' === $fun[0] ) {
+				$negate = true;
+				$fun = substr( $fun, 1 );
+			}
+
+			if ( $value && function_exists( 'is_' . $fun ) ) {
+				$call = call_user_func( 'is_' . $fun );
+				$conditions_hold = $conditions_hold || ( $negate ? ! $call : $call );
+			}
+		}
+
+		return $conditions_hold;
 	}
+
+
+	/**
+	 * Parse the autoplay options to determine if video should or should not
+	 * autoplay.
+	 *
+	 * @param  {assoic} $options
+	 * @return {bool}
+	 */
+	private static function parse_autoplay_options( $options, $ajax ) {
+		if ( empty( $options['autoplay'] ) ) {
+			return false;
+		}
+
+		if (
+			! empty( $options['autoplay']['always'] ) &&
+			$options['autoplay']['always']
+		) {
+			return true;
+		};
+
+		$mode = ! empty( $options['mode'] ) ? $options['mode'] : null;
+		//$ajax = defined( 'DOING_AJAX' ) && DOING_AJAX && $ajax;
+
+		if (
+			! empty( $options['autoplay']['lazy'] ) &&
+			$options['autoplay']['lazy'] &&
+			$ajax
+		) {
+			return true;
+		}
+
+		if (
+			! empty( $options['autoplay']['single'] ) &&
+			$options['autoplay']['single'] &&
+			is_single()
+		) {
+			return true;
+		}
+
+		return false;
+	}
+
+
 }
